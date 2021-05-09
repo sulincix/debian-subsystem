14a75aa25481f55fc3ea28a1a101791e7eda9297
Ali Rıza KESKİN <parduscix@yandex.ru>
2021-05-09 23:23:09 +0300

    d-term fullscreen

--- terminal/d-term.py
+++ terminal/d-term.py
@@ -7,20 +7,26 @@ terminal = Vte.Terminal()
 scrolled_window = Gtk.ScrolledWindow()
 terminal.connect("child_exited",Gtk.main_quit)
 cmd=""
-for i in sys.argv[1:]:
-    cmd+=i+" "
+if len(sys.argv) > 1:
+    if "--" not in sys.argv[1]:
+        cmd+=sys.argv[1]
+    for i in sys.argv[2:]:
+        cmd+=i+" "
 if cmd == "":
   cmd = os.environ["SHELL"]
 print(cmd)
-terminal.spawn_sync(Vte.PtyFlags.DEFAULT, None, ["sh","-c",cmd], [], GLib.SpawnFlags.DO_NOT_REAP_CHILD, None, None)
 win = Gtk.Window()
+if os.path.isfile("/usr/lib/sulin/dsl/debian.svg"):
+    win.set_icon_from_file("/usr/lib/sulin/dsl/debian.svg")
 win.connect('delete-event', Gtk.main_quit)
 win.add(scrolled_window)
 scrolled_window.add(terminal)
+if len(sys.argv) > 1 and sys.argv[1] == "--fullscreen":
+    win.fullscreen()
+else:
+    win.set_size_request(547,352)
 terminal.set_font_scale(0.9)
 win.set_title("d-term")
-if os.path.isfile("/usr/lib/sulin/dsl/debian.svg"):
-    win.set_icon_from_file("/usr/lib/sulin/dsl/debian.svg")
-win.set_size_request(547,352)
 win.show_all()
+terminal.spawn_sync(Vte.PtyFlags.DEFAULT, None, ["sh","-c",cmd], [], GLib.SpawnFlags.DO_NOT_REAP_CHILD, None, None)
 Gtk.main()
