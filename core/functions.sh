f8b740faf5905e2f2879c7531a3f7b24f1528540
Your Name <you@example.com>
2021-05-12 01:35:30 +0300

    fix

--- core/functions.sh
+++ core/functions.sh
@@ -5,7 +5,7 @@ wsl_block(){
     var=$(uname -r)
     [[ ! -f /proc/cpuinfo ]] && return 1
     if [[ "$var" == "*Microsoft*" || "$var" == "*microsoft*" || "$var" == "*WSL*" ]] \
-    || cat /proc/cpuinfo | grep "microcode" | grep "0xffffffff"
+    || cat /proc/cpuinfo | grep "microcode" | grep "0xffffffff" &>/dev/null
     then
         return 1
     fi
