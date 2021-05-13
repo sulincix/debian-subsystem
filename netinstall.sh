c901ab80c090d61abeeec9e5bf2be41fe786ffe2
Ali Rıza KESKİN <parduscix@yandex.ru>
2021-05-13 17:09:12 +0300

    fix archlinux installation

--- netinstall.sh
+++ netinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+CURDIR=$(pwd)
 cd /tmp
 git clone https://gitlab.com/sulincix/debian-subsystem
 cd debian-subsystem
 make && su -c "make install"
+cd ${CURDIR}
+rm -rf /tmp/debian-subsystem
