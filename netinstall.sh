f982e2de7343ca7a8774b803f951afcf35030db6
Ali Rıza KESKİN <ali.riza.keskin@pardus.org.tr>
2021-08-19 14:40:46 +0000

    update netinstall

--- netinstall.sh
+++ netinstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 CURDIR=$(pwd)
 cd /tmp
-if [[ -f /var/lib/dpkg/info ]] ; then
+if [[ -d /var/lib/dpkg/info ]] ; then
     su -c "apt install git wget make gcc -yq"
 fi
 git clone https://gitlab.com/sulincix/debian-subsystem
 cd debian-subsystem
 make && su -c "make install"
-if [[ -f /var/lib/dpkg/info ]] ; then
+if [[ -d /var/lib/dpkg/info ]] ; then
     su -c "make fix-debian"
 fi
 cd ${CURDIR}
