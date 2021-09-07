b8ed4c32f491fcac8c1b585634521c2280f27c4d
root <siz@e-posta.com>
2021-09-07 12:27:21 +0000

    droot rootmode

--- cli/droot.c
+++ cli/droot.c
@@ -3,8 +3,12 @@
 #include <string.h>
 #include <stdio.h>
 int main(int argc, char* argv[]){
+    if(getuid()!=0){
+        setenv("ROOTMODE","0",1);
+    }else{
+        setenv("ROOTMODE","1",1);
+    }
     setuid(0);
     setenv("USER","root",1);
-    setenv("ROOTMODE","0",1);
     execvp("/usr/lib/sulin/dsl/dsl.sh",NULL);
 }
