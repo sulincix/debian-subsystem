bb2bb4ebfa59a85d7a9b65d4332dff8cb464b018
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2021-11-10 08:30:07 +0000

    netinstall update

--- netinstall.sh
+++ netinstall.sh
@@ -4,6 +4,12 @@ cd /tmp
 if [[ -d /var/lib/dpkg/info ]] ; then
     su -c "apt install git wget make gcc -yq"
 fi
+# Install dummy selinux
+git clone https://gitlab.com/sulinos/devel/libselinux-dummy
+cd libselinux-dummy
+su -c "make && make install"
+cd /tmp
+# Install debian-subsystem
 git clone https://gitlab.com/sulincix/debian-subsystem
 cd debian-subsystem
 su -c "make && make build-extra && make install && make install-extra"
@@ -11,4 +17,4 @@ if [[ -d /var/lib/dpkg/info ]] ; then
     su -c "make fix-debian"
 fi
 cd ${CURDIR}
-rm -rf /tmp/debian-subsystem
+rm -rf /tmp/debian-subsystem /tmp/libselinux-dummy
