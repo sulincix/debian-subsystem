f17973e4601a0406fd9d50cc97369314d2e11b15
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2021-11-10 08:35:04 +0000

    netinstall update

--- netinstall.sh
+++ netinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 CURDIR=$(pwd)
 cd /tmp
 if [[ -d /var/lib/dpkg/info ]] ; then
@@ -6,15 +7,16 @@ if [[ -d /var/lib/dpkg/info ]] ; then
 fi
 # Install dummy selinux
 git clone https://gitlab.com/sulinos/devel/libselinux-dummy
-cd libselinux-dummy
-su -c "make && make install"
-cd /tmp
-# Install debian-subsystem
 git clone https://gitlab.com/sulincix/debian-subsystem
-cd debian-subsystem
-su -c "make && make build-extra && make install && make install-extra"
+
+cd /tmp/libselinux-dummy
+su -c "make && make install" &
+
+cd /tmp/debian-subsystem
+su -c "make && make build-extra && make install && make install-extra" 
 if [[ -d /var/lib/dpkg/info ]] ; then
     su -c "make fix-debian"
 fi
-cd ${CURDIR}
+
 rm -rf /tmp/debian-subsystem /tmp/libselinux-dummy
+cd ${CURDIR}
