75459acea977b985ed55c870059fce1912ad0b48
sulincix <parduscix@yandex.ru>
2024-06-04 21:02:06 +0000

    pam module cgroup init

--- src/pam/module.c
+++ src/pam/module.c
@@ -2,9 +2,15 @@
 #include <security/pam_modules.h>
 #include <security/pam_ext.h>
 #include <sys/stat.h>
-
+#include <stdbool.h>
 
 #include <lsl.h>
+
+static bool is_running(){
+     struct stat st;
+     stat("/sys/fs/cgroup/debian/cgroup.procs", &st);
+     return st.st_size != 0;
+}
 
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     mode_t u = umask(0022);
@@ -17,6 +23,9 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, cons
 }
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-    umount_run_user();
+    create_dir("/sys/fs/cgroup/debian");
+    if(!is_running()){
+        umount_all();
+    }
     return PAM_SUCCESS;
 }
