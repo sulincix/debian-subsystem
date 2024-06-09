c072c39e9281b921fd707fdccf822e160ed353d0
sulincix <parduscix@yandex.ru>
2024-06-09 22:57:15 +0000

    security fixes

--- src/cli/lsl.c
+++ src/cli/lsl.c
@@ -1,4 +1,8 @@
-#include <lsl.h>
+#include <dlfcn.h>
+#include <stddef.h>
+static int (*debrun_main)(int, char**);
 int main(int argc, char** argv) {
+    void* handle = dlopen(NULL, RTLD_LAZY);
+    debrun_main = dlsym(handle, "debrun_main");
     return debrun_main(argc, argv);
 }
--- src/sandbox.c
+++ src/sandbox.c
@@ -12,11 +12,21 @@ static char child_stack[1024*1024];
 
 static char* command;
 static char** args;
-static int execsnd(){
-    (void)sethostname("sandbox", 7);
-    mount("proc", "/proc", "proc", 0, NULL);
-    (void)setuid(cur_uid);
+static int execsnd() {
+    if (sethostname("sandbox", 7) != 0) {
+        perror("sethostname");
+        return -1;
+    }
+    if (mount("proc", "/proc", "proc", 0, NULL) != 0) {
+        perror("mount");
+        return -1;
+    }
+    if (setuid(cur_uid) != 0) {
+        perror("setuid");
+        return -1;
+    }
     execvp(command, args);
+    perror("execvp");
     return -1;
 }
 
@@ -24,6 +34,7 @@ void execute_sandbox(char* cmd, char** argv){
     if(getenv("LSL_NOSANDBOX") != NULL){
         (void)setuid(cur_uid);
         execvp(cmd, argv);
+        perror("execvp");
     }
     size_t i=0;
     for(i=0;argv[i] != NULL; i++);
@@ -35,6 +46,16 @@ void execute_sandbox(char* cmd, char** argv){
     pid_t pid = clone(execsnd, 
         child_stack+1024*1024, CLONE_NEWPID | CLONE_NEWUTS | CLONE_NEWNS |
              CLONE_NEWIPC | CLONE_VM | CLONE_VFORK | SIGCHLD , NULL
-     );
-    waitpid(pid, NULL, 0);
+    );
+    if (pid == -1) {
+        perror("clone");
+        exit(EXIT_FAILURE);
+    }
+    int status;
+    waitpid(pid, &status, 0);
+    for (size_t j = 0; j < i; j++) {
+        free(args[j]);
+    }
+    free(args);
+    exit(status / 256);
 }
