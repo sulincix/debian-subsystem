f80eb30d63f004892617c27cab0afcd9fd03fa95
sulincix <parduscix@yandex.ru>
2024-08-02 00:15:34 +0300

    fix arch init

--- distro/archlinux/subsystem-init.sh
+++ distro/archlinux/subsystem-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=/usr/bin:/usr/sbin:/bin:/sbin
 set -e
 set -o pipefail
 mkdir -p /var/lib/subsystem/
@@ -7,7 +8,9 @@ wget https://geo.mirror.pkgbuild.com/iso/latest/archlinux-bootstrap-x86_64.tar.z
 tar -xvf -
 mv root.x86_64/* ./
 rm -rf pkglist.x86_64.txt
+cat /etc/resolv.conf > /var/lib/subsystem/etc/resolv.conf
 sed -i "s|#Server = https://geo.mirror.pkgbuild.com|Server = https://geo.mirror.pkgbuild.com|g" /var/lib/subsystem/etc/pacman.d/mirrorlist
 sed -i "s/^CheckSpace/#CheckSpace/g" /var/lib/subsystem/etc/pacman.conf
 sed -i "s/#ParallelDownloads/ParallelDownloads/g" /var/lib/subsystem/etc/pacman.conf
 chroot /var/lib/subsystem/ pacman-key --init
+chroot /var/lib/subsystem/ pacman-key --populate
